52d23092a6860716eb3416c41bc022b75ec96687
bhusan thapa <shumsher@bhusans-MacBook-Pro.local>
2024-11-28 19:13:02 +0545

    spinner

--- frontend/src/features/registration/GenerateForm.jsx
+++ frontend/src/features/registration/GenerateForm.jsx
@@ -6,38 +6,63 @@ export default function GenerateForm(){
         generate();
       }
       if(!isLoading){
-        <div className="content container-fluid">
-                <div className="page-header">
-                <div className="row align-items-center">
-                <div className="col-sm-12">
-                
-                </div>
-                </div>
-                </div>
-    
-                <div className="row">
-                <div className="col-sm-12">
-                <div className="card comman-shadow">
-                <div className="card-body">
-                <div className="row">
-                <div className="col-12">
-                <h5 className="form-title student-info">Generate Form<span></span></h5>
-                <h6>Please click Generate form</h6>
-                </div>
-                <form>
-              <button 
-                type="button" 
-                class="btn btn-primary"
-                onClick={onSubmit}>Generate</button>
-             </form>
-            </div>
-            </div>
-            </div>
-            </div>
-                </div>
-         </div>
-      }
-    return(
+        return(<div className="content container-fluid">
+        <div className="page-header">
+        <div className="row align-items-center">
+        <div className="col-sm-12">
         
-    );
+        </div>
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="col-sm-12">
+        <div className="card comman-shadow">
+        <div className="card-body">
+        <div className="row">
+        <div className="col-12">
+        <h5 className="form-title student-info">Generate Form<span></span></h5>
+        <h6>Please click Generate form</h6>
+        </div>
+        <form>
+      <button 
+        type="button" 
+        class="btn btn-primary"
+        onClick={onSubmit}>Generate</button>
+     </form>
+    </div>
+    </div>
+    </div>
+    </div>
+        </div>
+ </div>
+ );
+        }else{
+          rerturn(<div className="content container-fluid">
+          <div className="page-header">
+          <div className="row align-items-center">
+          <div className="col-sm-12">
+          
+          </div>
+          </div>
+          </div>
+
+          <div className="row">
+          <div className="col-sm-12">
+          <div className="card comman-shadow">
+          <div className="card-body">
+          <div className="row">
+          <div className="col-12">
+          </div>
+          <Spinner/>
+      </div>
+      </div>
+      </div>
+      </div>
+          </div>
+   </div>
+   );
+        
+      }
+    
 }
