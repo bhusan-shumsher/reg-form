11e5644aada6d4373ce64ea1fa2dba05aa125597
bhusan thapa <shumsher@bhusans-MacBook-Pro.local>
2024-11-28 20:46:08 +0545

    test

--- frontend/src/features/registration/GenerateForm.jsx
+++ frontend/src/features/registration/GenerateForm.jsx
@@ -1,9 +1,38 @@
+import {useState} from 'react';
 import { useGenerateForm } from "../../hooks/registrationHooks/useGenerateForm";
 import Spinner from '../../ui/Spinner';
 export default function GenerateForm(){
+    const [clicked,setClicked] = useState(false);
     const {generate,isLoading} = useGenerateForm();
     const onSubmit = () => {
+      setClicked(true);
         generate();
+      }
+      if(clicked){
+        rerturn(<div className="content container-fluid">
+          <div className="page-header">
+          <div className="row align-items-center">
+          <div className="col-sm-12">
+          
+          </div>
+          </div>
+          </div>
+
+          <div className="row">
+          <div className="col-sm-12">
+          <div className="card comman-shadow">
+          <div className="card-body">
+          <div className="row">
+          <div className="col-12">
+          </div>
+          <Spinner/>
+      </div>
+      </div>
+      </div>
+      </div>
+          </div>
+   </div>
+   );
       }
       if(!isLoading){
         return(<div className="content container-fluid">
@@ -28,7 +57,7 @@ export default function GenerateForm(){
       <button 
         type="button" 
         class="btn btn-primary"
-        onClick={onSubmit}>{!isLoading ? 'Generate' : <Spinner/>}</button>
+        onClick={onSubmit}> Generate</button>
      </form>
     </div>
     </div>
