ae1d6eef24b23e3e0a2f1ba0e1af89096e330497
bhusan thapa <shumsher@bhusans-MacBook-Pro.local>
2025-07-05 22:52:31 +0545

    lucky

--- server/src/routes/ftp.js
+++ server/src/routes/ftp.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const serveIndex = require('serve-index');
 const isAdmin = require('../middlewares/is-admin')
-const submittedFormPath = path.join('/usr/app/src/files/submitted-form');
+// const submittedFormPath = path.join('/usr/app/src/files/submitted-form');
 
 router.use('/api/9818911707',
         express.static('src/files/submitted-form/'),
