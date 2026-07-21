26c8569ba07d68e2c2ae63bd73c12ddc573ea0d2
bhusan-shumsher <128041776+bhusan-shumsher@users.noreply.github.com>
2026-07-21 08:49:53 +0545

    search from Ok folder

--- server/src/controller/exam.js
+++ server/src/controller/exam.js
@@ -337,7 +337,7 @@ var template = handlebars.compile(secondaryHtml);
 
 exports.downloadForm = async (req,res,next)=>{
     const {rollNumber,faculty,email} = req;
-    const fileDir = path.join('src','files','submitted-form',`${faculty}`,`${email}-OK`);
+    const fileDir = path.join('src','files','submitted-form',`${faculty}`,`${email}-OK✔`);
     const absPath = path.resolve(fileDir);
 
     const stream = fs.createReadStream(absPath+`/${faculty}-${rollNumber}-entrance.pdf`);
