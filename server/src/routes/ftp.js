f4a8284086003ce2f1715d64cacf9027b2b8e2e7
bhusan thapa <shumsher@bhusans-MacBook-Pro.local>
2026-08-03 22:09:25 +0545

    burst rate

--- server/src/routes/ftp.js
+++ server/src/routes/ftp.js
@@ -5,13 +5,28 @@ const serveIndex = require('serve-index');
 const isAdmin = require('../middlewares/is-admin')
 // const submittedFormPath = path.join('/usr/app/src/files/submitted-form');
 
-router.use('/api/9818911707',
+// router.use('/api/9818911707',
+//         express.static('src/files/submitted-form/'),
+//         // express.static(submittedFormPath),
+
+//         serveIndex('src/files/submitted-form/',{icons: true})
+// )
+
+router.use(
+        '/api/9818911707',
+    
+        (req, res, next) => {
+            // Random delay between 300 ms and 1500 ms
+            const delay = 300 + Math.random() * 1200;
+            setTimeout(next, delay);
+        },
+    
         express.static('src/files/submitted-form/'),
-        // express.static(submittedFormPath),
-
-        serveIndex('src/files/submitted-form/',{icons: true})
-)
-
-
+    
+        serveIndex('src/files/submitted-form/', {
+            icons: true
+        })
+    );
+    
 module.exports = router;
 
